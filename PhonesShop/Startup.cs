abce9120dfa9628d63b96b0b3b91dfeb408439c4
pupil <pupil@ivkhk.ee>
2023-03-21 12:10:41 +0200

    Update

---
+++ PhonesShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhonesShop.Startup))]
+namespace PhonesShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
